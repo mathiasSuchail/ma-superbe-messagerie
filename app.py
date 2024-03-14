0efabac78df7fb57b24a3fcd3099f8b578fb00ba
Mathias SUCHAIL <mathias.suchail@yahoo.com>
2024-03-14 12:39:20 +0100

    :rocket: front messagerie presque ok

--- app.py
+++ app.py
@@ -8,29 +8,29 @@ app = Flask(__name__)
 import sqlite3
 from flask import g
 
-DATABASE = '/database.db'
+
+import sqlite3
+from flask import g
+
+DATABASE = './db/database.db'
+
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('initBdd.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
 
 #ROUTING
 @app.route("/")
 def index():
+    cur = get_db().cursor()
     return render_template("dashboard.html")
 
 @app.route("/register")
@@ -41,6 +41,6 @@ def register():
 def login():
     return render_template("auth/login.html")
 
-@app.route("/messages")
+@app.route("/mailbox")
 def messages():
-    return render_template("messages.html")
+    return render_template("mailbox.html")
